ec7772b86ef5f4aea1af702075a4d95919c39f24
Kelsea <andersonkelsea@gmail.com>
2016-03-14 21:55:14 -0400

    Fizz Buzz Refactor

--- js/app.js
+++ js/app.js
@@ -1,21 +1,35 @@
-for (var i = 1; i <= 100; i++){
-	if(i % 3 == 0 && i % 5 == 0){
-		console.log("fizz buzz");
-		$('body').append("<p>fizz buzz</p>");
-	}
-	else if (i % 5 == 0){
-		console.log("buzz");
-		$('body').append("<p>buzz</p>");
-
-	}
-	else if (i % 3 == 0){
-		console.log("fizz");
-		$('body').append("<p>fizz</p>");
-
+$(document).ready(function(){
+	var userFeedback = prompt("Choose a number between 1 and 100");
+	var userNumber = parseInt(userFeedback);
+	if (isInt(userNumber) == true){
+		fizzBuzz(userNumber);
 	}
 	else{
-		console.log(i);
-		$('body').append("<p>" + i + "</p>");
-
+		alert("Please pick a number between 1 and 100");
+	}
+})
+function isInt(value) {
+  return !isNaN(value) && 
+         parseInt(Number(value)) == value && 
+         !isNaN(parseInt(value, 10));
+}
+function fizzBuzz (limit){
+	for (var i = 1; i <= limit; i++){
+		if(i % 15 == 0){
+			console.log("fizz buzz");
+			$('body').append("<p>fizz buzz</p>");
+		}
+		else if (i % 5 == 0){
+			console.log("buzz");
+			$('body').append("<p>buzz</p>");
+		}
+		else if (i % 3 == 0){
+			console.log("fizz");
+			$('body').append("<p>fizz</p>");
+		}
+		else{
+			console.log(i);
+			$('body').append("<p>" + i + "</p>");
+		}
 	}
 }
